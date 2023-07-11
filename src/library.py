50eac292f3dc9c92bc90121605225108a094d5b6
annassanchez <annassanchez@gmail.com>
2023-07-11 16:04:28 +0200

    fixed code for gini

--- src/library.py
+++ src/library.py
@@ -3,10 +3,10 @@ ine_experimental = {
     'Distribución por fuente de ingresos':'30825',
     'Porcentaje de población con ingresos por unidad de consumo por debajo de determinados umbrales fijos por sexo':'30826',
     'Porcentaje de población con ingresos por unidad de consumo por debajo de determinados umbrales fijos por sexo y tramos de edad':'30827',
-    'Porcentaje de población con ingresos por unidad de consumo por debajo de determinados umbrales fijos por sexo y nacionalidad ':'30828',
+    'Porcentaje de población con ingresos por unidad de consumo por debajo de determinados umbrales fijos por sexo y nacionalidad':'30828',
     'Porcentaje de población con ingresos por unidad de consumo por debajo/encima de determinados umbrales relativos por sexo':'30829',
     'Porcentaje de población con ingresos por unidad de consumo por debajo/encima de determinados umbrales relativos por sexo y tramos de edad':'30830',
     'Porcentaje de población con ingresos por unidad de consumo por debajo/encima de determinados umbrales relativos por sexo y nacionalidad':'30831',
-    'Índice de Gini y Distribución de la renta P80/P20':'30832',
-    'Indicadores demográficos':'30833'
+    'Índice de Gini y Distribución de la renta P80/P20':'37677',
+    'Indicadores demográficos':'30832'
     }
